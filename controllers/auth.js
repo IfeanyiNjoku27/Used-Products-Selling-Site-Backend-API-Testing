41b173b6b18daca604eb689d4c0c37c165b03cea
Christann Groves <cgroves6@my.centennialcollege.ca>
2025-12-06 15:09:35 -0500

    Refactor auth methods and update profile logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,85 +1,105 @@
-const UserModel = require('../models/users');
-const jwt = require('jsonwebtoken');
-const config = require('../config/config');
-const { expressjwt } = require('express-jwt');
-
-module.exports.signup = async function (req, res, next) {
-  try {
-    const { username, email, password } = req.body;
-
-    let existing = await UserModel.findOne({ email });
-    if (existing) {
-      res.status(400);
-      throw new Error("Email already registered.");
-    }
-
-    let user = new UserModel({ username, email, password });
-    let savedUser = await user.save();
-
-    res.status(201).json({
-      success: true,
-      message: "User registered successfully",
-      user: {
-        id: savedUser._id,
-        username: savedUser.username,
-        email: savedUser.email,
-      }
-    });
-
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-};
+let UserModel = require('../models/users');
+let jwt = require('jsonwebtoken');
+let config = require('../config/config');
 
 module.exports.signin = async function (req, res, next) {
-  try {
-    let user = await UserModel.findOne({ email: req.body.email });
+    try {
+        let user = await UserModel.findOne({ email: req.body.email });
+        if (!user) {
+            res.status(400);
+            throw new Error("User not found");
+        }
 
-    if (!user) {
-      res.status(404);
-      throw new Error("User not found");
+        if (!user.authenticate(req.body.password)) {
+            res.status(400);
+            throw new Error("Email and/or Password do not match");
+        }
+
+        let payload = {
+            id: user._id,
+            username: user.username
+        };
+
+        let token = jwt.sign(payload, config.SECRETKEY, {
+            algorithm: "HS512",
+            expiresIn: "20min"
+        });
+
+        res.json({
+            success: true,
+            message: "User authenticated successfully",
+            token: token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+        
+    } catch (error) {
+        next(error);
     }
-
-    if (!user.authenticate(req.body.password)) {
-      res.status(400);
-      throw new Error("Email and/or Password do not match.");
-    }
-
-    const payload = {
-      id: user._id,
-      username: user.username,
-    };
-
-    const token = jwt.sign(payload, config.SECRETKEY, {
-      algorithm: 'HS512',
-      expiresIn: "20min"
-    });
-
-    res.json({
-      success: true,
-      message: "User authenticated successfully.",
-      token: token,
-      user: {
-        id: user._id,
-        username: user.username,
-        email: user.email
-      }
-    });
-
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 };
 
-module.exports.requireSign = expressjwt({
-  secret: config.SECRETKEY,
-  algorithms: ['HS512'],
-  requestProperty: "auth",
-});
 
-module.exports.logtoken = async function (req, res, next) {
-  console.log("Headers: ", req.headers);
-  next();
+module.exports.signup = async function (req, res, next) {
+    try {
+        let existing = await UserModel.findOne({ email: req.body.email });
+        if (existing) {
+            res.status(400);
+            throw new Error("Email already registered");
+        }
+
+        let user = new UserModel(req.body);
+        await user.save();
+
+        res.json({
+            success: true,
+            message: "User registered successfully"
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+module.exports.profile = async function (req, res, next) {
+    try {
+        let user = await UserModel.findById(req.user._id).select("-password");
+        if (!user) {
+            res.status(404);
+            throw new Error("User not found");
+        }
+
+        res.json(user);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+module.exports.updateProfile = async function (req, res, next) {
+    try {
+        let user = await UserModel.findById(req.user._id);
+
+        if (!user) {
+            res.status(404);
+            throw new Error("User not found");
+        }
+
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+
+        await user.save();
+
+        res.json({
+            success: true,
+            message: "Profile updated successfully"
+        });
+
+    } catch (error) {
+        next(error);
+    }
 };
